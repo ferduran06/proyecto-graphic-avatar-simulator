8b3bceaaddf52c3d942c728096082fe1baab4a28
ferduran06 <ma2100@estudiantec.cr>
2020-07-08 21:21:39 -0600

    Creaciones de ventanas y eleccion de vestuario

--- proyecto3-Fernanda-Veronica.py
+++ proyecto3-Fernanda-Veronica.py
@@ -60,19 +60,68 @@ class Vestuario:
 
 
 def createAvatar(ventana):
-
     ventana_create = Toplevel()
     ventana_create.config(bg = "pink")
     ventana_create.title("Create Avatar")
     ventana_create.geometry("500x400")
-    label1 = Label(ventana_create, bg="pink", text="Graphic Avatar Simulator", font="Time, 20").pack(padx=20, pady=40)
-    label3 = Label(ventana_create, bg="pink", text="CREATE AVATAR", font="Arial, 15").pack()
+    Label(ventana_create, bg="pink", text="Graphic Avatar Simulator", font="Time, 20").pack(padx=20, pady=40)
+    Label(ventana_create, bg="pink", text="CREATE AVATAR", font="Arial, 15").pack()
+    ventana.mainloop()
+
+def dressAccesory(ventana):
+    vestuarios = Vestuario()
+    c = 0
+    ventana_accesory = Toplevel()
+    ventana_accesory.config(bg="pink")
+    ventana_accesory.title("Create Avatar")
+    ventana_accesory.geometry("600x550")
+    Label(ventana_accesory, bg="pink", text="Graphic Avatar Simulator", font="Time, 20").pack(padx=20, pady=40)
+    Label(ventana_accesory, bg="pink", text="DRESS UP AVATAR", font="Arial, 15").pack()
+    Label(ventana_accesory, bg="pink", text="Accessories ", font="Arial, 13").pack(padx=10, pady=10)
+    for i in vestuarios.getAccesorios():
+        Radiobutton(ventana_accesory, bg="pink", text=i, value=c, font="Arial, 12").pack()
+        c += 1
+    Button(ventana_accesory, text="Next", command=lambda: dressShoes(ventana_accesory), font="Arial, 11").pack(padx=10,
+                                                                                                             pady=20)
+    ventana.withdraw()
+
+
+def dressShoes(ventana):
+    vestuarios = Vestuario()
+    c = 0
+    ventana_shoes = Toplevel()
+    ventana_shoes.config(bg="pink")
+    ventana_shoes.title("Create Avatar")
+    ventana_shoes.geometry("600x550")
+    Label(ventana_shoes, bg="pink", text="Graphic Avatar Simulator", font="Time, 20").pack(padx=20, pady=40)
+    Label(ventana_shoes, bg="pink", text="DRESS UP AVATAR", font="Arial, 15").pack()
+    Label(ventana_shoes, bg="pink", text="Shoes ", font="Arial, 13").pack(padx=10, pady=10)
+    for i in vestuarios.getCalzado():
+        Radiobutton(ventana_shoes, bg="pink", text=i, value=c, font="Arial, 12").pack()
+        c += 1
+    Button(ventana_shoes, text="Next", command=lambda: dressAccesory(ventana_shoes), font="Arial, 11").pack(padx=10,pady=20)
+    ventana.withdraw()
+
+
+def dressOutfit(ventana):
+    vestuarios = Vestuario()
+    c = 0
+    ventana_outfit = Toplevel()
+    ventana_outfit.config(bg = "pink")
+    ventana_outfit.title("Create Avatar")
+    ventana_outfit.geometry("600x550")
+    Label(ventana_outfit, bg="pink", text="Graphic Avatar Simulator", font="Time, 20").pack(padx=20, pady=40)
+    Label(ventana_outfit, bg="pink", text="DRESS UP AVATAR", font="Arial, 15").pack()
+    Label(ventana_outfit, bg="pink", text="Outfit ", font="Arial, 13").pack(padx=10, pady=10)
+    for i in vestuarios.getRopa():
+        Radiobutton(ventana_outfit,bg="pink",text=i,value=c,font="Arial, 12").pack()
+        c += 1
+    Button(ventana_outfit, text="Next", command=lambda: dressAccesory(ventana_outfit),font="Arial, 11").pack(padx=10, pady=20)
     ventana.withdraw()
 
 def ingresar(v,e1,e2, nombre,contrasenna):
 
     if nombre == "fer" and contrasenna =="123":
-
         ventana_ingresar = Toplevel()
         ventana_ingresar.config(background="pink")
         ventana_ingresar.title("Administrator")
@@ -81,7 +130,7 @@ def ingresar(v,e1,e2, nombre,contrasenna):
         saludo = ("Welcome " + nombre)
         label3 = Label(ventana_ingresar,bg = "pink", text=saludo,font="Arial, 15").pack()
         Button(ventana_ingresar, text="Create Avatar",command = lambda:createAvatar(ventana_ingresar),font="Arial, 11").pack(padx=10, pady=20)
-        Button(ventana_ingresar, text="Dress up Avatar", font="Arial, 11").pack(padx=10, pady=20)
+        Button(ventana_ingresar, text="Dress up Avatar",command = lambda:dressOutfit(ventana_ingresar),font="Arial, 11").pack(padx=10, pady=20)
         v.withdraw()
 
     else:
@@ -113,15 +162,15 @@ def ventanaPrincipal():
     # imagen = Label(ventana, image=direccion).place(x=0, y=0)  # .place(x,y) uno le dice en qué posición lo quiere
 
     #TITULO
-    label1 = Label(ventana, bg="pink", text="Welcome Graphic Avatar Simulator", font="Time, 20").pack(padx=20,pady=40)  #.pack() lo centra
+    label1 = Label(ventana, background="pink", text="Welcome Graphic Avatar Simulator", font="Time, 20").pack(padx=20,pady=40)  #.pack() lo centra
 
     #DIGITAR EL USUARIO
-    label2 = Label(ventana, bg="pink",text="User", font="Arial, 15").pack(padx=5, pady=5)  # .pack() lo centra
+    Label(ventana, background="pink",text="User", font="Arial, 15").pack(padx=5, pady=5)  # .pack() lo centra
     entry_nombre_usuario = Entry(ventana, font="Arial, 12")
     entry_nombre_usuario.pack(padx=5, pady=5)
 
     #DIGITAR LA CONTRASEÑA
-    label3 = Label(ventana, bg="pink", text="Password", font="Arial, 15").pack(padx=5, pady=5)  # .pack() lo centra
+    Label(ventana, background="pink", text="Password", font="Arial, 15").pack(padx=5, pady=5)  # .pack() lo centra
     entry_contraseña = Entry(ventana, show="*", font="Arial, 12")
     entry_contraseña.pack(padx=5, pady=5)
 
