e1663b94403667879c0b706691cdd403dca685bf
ferduran06 <ma2100@estudiantec.cr>
2020-07-27 23:30:40 -0600

    CORRECCIONES DE CLASES

--- proyecto3-Fernanda-Veronica.py
+++ proyecto3-Fernanda-Veronica.py
@@ -2,7 +2,7 @@ from tkinter import *
 import os
 from tkinter import messagebox as MessageBox
 
-path = 'C:/Users/hp/Documents/GitHub/proyecto-graphic-avatar-simulator/AVATARS'
+path = 'C:/Users/ma210/OneDrive/Documents/GitHub/proyecto-graphic-avatar-simulator/AVATARS/'
 files = os.listdir(path)
 
 
@@ -41,18 +41,6 @@ class Vestuario:
     def getCalzado(self):  # Get that gets the shoe in X position
         return self.vestuario["calzado"]
 
-class Rostro:
-    def __init__(self):
-        self.rostro = ["Redondo", "Alargado", "Corazón", "Cuadrado", "Ovalado", "Rectangular"]  # Attributes assigned
-
-    def set(self, rostro):
-        self.rostro = rostro
-
-    def getRostro(self):
-        return self.rostro
-
-    def getRostroIndice(self, indice):
-        return self.rostro[indice]
 
 class Piel:
     def __init__(self):
@@ -80,19 +68,18 @@ class Cabello:
     def getTexturaIndice(self, indice):
         return self.cabello[indice]
 
-class Ojos:
+class ColorCabello:
     def __init__(self):
-        self.ojos = ["Almendrados", "Separados", "Redondos", "Caídos", "Saltones", "Juntos", "Profundos",
-                     "Asiático"]  # Attributes assigned
+        self.setColorCabello = ["Negro", "Castaño", "Rubia", "Rojizo"]  # Attributes assigned
 
-    def setFormaOjos(self, formaOjos):
-        self.ojos = formaOjos  # Assign value in the dictionary in the eye shape position
+    def setColorCabello(self, colorCabello):
+        self.colorCabello = colorCabello  # Assign value in the dictionary in the eye shape position
 
-    def getFormaOjoa(self):  # Get that gets the eye shape in X position
-        return self.ojos
+    def getColorCabello(self):  # Get that gets the eye shape in X position
+        return self.colorCabello
 
-    def getOjosIndice(self, indice):
-        return self.ojos[indice]
+    def getColorCabelloIndice(self, indice):
+        return self.colorCabello[indice]
 
 class Genero:
     def __init__(self):
@@ -154,7 +141,7 @@ def admin(v,nombre):
         ventana_ingresar.destroy()
         v.deiconify()
 
-<<<<<<< HEAD
+
     Button(v, text="Exit", font="Arial,11", command=lambda: regresar()).pack(padx=0, pady=20)
     v.withdraw()
 
@@ -170,28 +157,19 @@ def analista(v,nombre):
     Button(ventana_ingresar, text="Report 1", font="Arial, 11").pack(padx=10, pady=20)
     Button(ventana_ingresar, text="Report 2", font="Arial, 11").pack(padx=10, pady=20)
     v.destroy()
-=======
-        ventana_ingresar = Toplevel()
-        ventana_ingresar.config(background="pink")
-        ventana_ingresar.title("Analyst")
-        ventana_ingresar.geometry("500x400")
 
-        Label(ventana_ingresar, bg="pink", text="Graphic Avatar Simulator", font="Time, 20").pack(padx=20,pady=40)
-        saludo = ("Welcome " + nombre)
-        Label(ventana_ingresar, bg="pink", text=saludo, font="Arial, 15").pack()
-        Button(ventana_ingresar, text="Report 1", command= lambda:mostrar_Consulta1(ventana_ingresar),font="Arial, 11").pack(padx=10, pady=20)
-        Button(ventana_ingresar, text="Report 2", command= lambda:mostrar_Consulta2(ventana_ingresar),font="Arial, 11").pack(padx=10, pady=20)
-        v.withdraw()
-    else:
-        MessageBox.showerror("Error","INVALID DATA. TRY AGAIN")
->>>>>>> 0e62a5ca232b38ee19ec744f1edf676de09fb05b
+    ventana_ingresar = Toplevel()
+    ventana_ingresar.config(background="pink")
+    ventana_ingresar.title("Analyst")
+    ventana_ingresar.geometry("500x400")
 
-    def regresar():
-        ventana_ingresar.destroy()
-        v.deiconify()
-
-    Button(v, text="Exit", font="Arial,11", command=lambda: regresar()).pack(padx=0, pady=20)
+    Label(ventana_ingresar, bg="pink", text="Graphic Avatar Simulator", font="Time, 20").pack(padx=20,pady=40)
+    saludo = ("Welcome " + nombre)
+    Label(ventana_ingresar, bg="pink", text=saludo, font="Arial, 15").pack()
+    Button(ventana_ingresar, text="Report 1", command= lambda:mostrar_Consulta1(ventana_ingresar),font="Arial, 11").pack(padx=10, pady=20)
+    Button(ventana_ingresar, text="Report 2", command= lambda:mostrar_Consulta2(ventana_ingresar),font="Arial, 11").pack(padx=10, pady=20)
     v.withdraw()
+
 
 def ingresar(v,e1,e2, nombre,contrasenna):
 
@@ -220,34 +198,12 @@ def createAvatar(ventana):
                                                                                                                pady=10)
     cedula = Entry(ventana_create, font="Arial, 13")
     cedula.pack(padx=10, pady=10)
-    Button(ventana_create, command =lambda :registerFace(ventana,cedula,cedula.get()),text="Next",font="Arial, 11").pack(padx=10,pady=20)
+    Button(ventana_create, command =lambda :registerSkin(ventana,cedula,cedula.get()),text="Next",font="Arial, 11").pack(padx=10,pady=20)
 
     ventana.withdraw()
 
-def registerFace(v,c,cedula):
-    ventana_face = Toplevel()
-    ventana_face.config(bg = "pink")
-    ventana_face.title("Create Avatar")
-    ventana_face.geometry("500x500")
 
-    selected = IntVar()
-    creaciontxt(cedula)
-
-    haces = Rostro()
-    c = 0
-
-    Label(ventana_face, bg="pink", text="Graphic Avatar Simulator", font="Time, 20").pack(padx=20, pady=40)
-    Label(ventana_face, bg="pink", text="FACE SHAPES AVATAR", font="Arial, 15").pack()
-    Label(ventana_face, bg="pink", text="FACE ", font="Arial, 14").pack(padx=10, pady=10)
-    Label(ventana_face, bg="pink", text="Select an option ", font="Arial, 13").pack(padx=10, pady=10)
-
-    for i in haces.getRostro():
-        Radiobutton(ventana_face,bg="pink",text=i,variable=selected,value=c,font="Arial, 12").pack()
-        c += 1
-    Button(ventana_face, command =lambda :registerSkin(ventana_face,cedula,selected.get()),text="Next",font="Arial, 11").pack(padx=10,pady=20)
-    v.destroy()
-
-def registerSkin(ventana,cedula,faces):
+def registerSkin(ventana,cedula):
     skin = Piel()
     c = 0
     selected = IntVar()
@@ -266,11 +222,11 @@ def registerSkin(ventana,cedula,faces):
         Radiobutton(ventana_skin,bg="pink",text=i,variable=selected,value=c,font="Arial, 12").pack()
         c += 1
 
-    Button(ventana_skin, command=lambda: registerHair(ventana_skin, cedula, faces,selected.get()), text="Next",
+    Button(ventana_skin, command=lambda: registerHair(ventana_skin, cedula, selected.get()), text="Next",
            font="Arial, 11").pack(padx=10, pady=20)
     ventana.destroy()
 
-def registerHair(ventana,cedula,faces,skin):
+def registerHair(ventana,cedula,skin):
 
     hair = Cabello()
     c = 0
@@ -287,35 +243,35 @@ def registerHair(ventana,cedula,faces,skin):
     for i in hair.getTextura():
         Radiobutton(ventana_hair,bg="pink",text=i,variable=selected,value=c,font="Arial, 12").pack()
         c += 1
-    Button(ventana_hair, command=lambda: registerEyes(ventana_hair, cedula, faces, skin,selected.get()), text="Next",
+    Button(ventana_hair, command=lambda: registerEyes(ventana_hair, cedula, skin,selected.get()), text="Next",
            font="Arial, 11").pack(padx=10, pady=20)
     ventana.destroy()
 
 
-def registerEyes(ventana,cedula,faces,skin,hair):
+def registerEyes(ventana,cedula,skin,hair):
 
-    eyes = Ojos()
+    colorHair = ColorCabello()
     c = 0
 
     selected = IntVar()
 
-    ventana_eyes = Toplevel()
-    ventana_eyes.config(bg = "pink")
-    ventana_eyes.title("Create Avatar")
-    ventana_eyes.geometry("600x600")
+    ventana_hairs = Toplevel()
+    ventana_hairs.config(bg = "pink")
+    ventana_hairs.title("Create Avatar")
+    ventana_hairs.geometry("600x600")
 
-    Label(ventana_eyes, bg="pink", text="Graphic Avatar Simulator", font="Time, 20").pack(padx=20, pady=40)
-    Label(ventana_eyes, bg="pink", text="EYE SHAPE AVATAR", font="Arial, 15").pack()
-    Label(ventana_eyes, bg="pink", text="SHAPE ", font="Arial, 14").pack(padx=10, pady=10)
-    Label(ventana_eyes, bg="pink", text="Select an option ", font="Arial, 13").pack(padx=10, pady=10)
-    for i in eyes.getFormaOjoa():
-        Radiobutton(ventana_eyes,bg="pink",text=i,variable=selected,value=c,font="Arial, 12").pack()
+    Label(ventana_hairs, bg="pink", text="Graphic Avatar Simulator", font="Time, 20").pack(padx=20, pady=40)
+    Label(ventana_hairs, bg="pink", text="EYE SHAPE AVATAR", font="Arial, 15").pack()
+    Label(ventana_hairs, bg="pink", text="SHAPE ", font="Arial, 14").pack(padx=10, pady=10)
+    Label(ventana_hairs, bg="pink", text="Select an option ", font="Arial, 13").pack(padx=10, pady=10)
+    for i in colorHair.getColorCabello():
+        Radiobutton(ventana_hairs,bg="pink",text=i,variable=selected,value=c,font="Arial, 12").pack()
         c += 1
-    Button(ventana_eyes, command=lambda: registerGender(ventana_eyes, cedula, faces,skin,hair, selected.get()), text="Next",
+    Button(ventana_hairs, command=lambda: registerGender(ventana_hairs, cedula,skin,hair, selected.get()), text="Next",
            font="Arial, 11").pack(padx=10, pady=20)
     ventana.destroy()
 
-def registerGender(ventana,cedula,faces,skin,hair,eyes):
+def registerGender(ventana,cedula,skin,hair,eyes):
 
     gender = Genero()
     c = 0
@@ -332,7 +288,7 @@ def registerGender(ventana,cedula,faces,skin,hair,eyes):
     for i in gender.getGenero():
         Radiobutton(ventana_gender,bg="pink",text=i,variable=selected,value=c,font="Arial, 12").pack()
         c += 1
-    Button(ventana_gender, command=lambda: mostrarInfor(ventana_gender, cedula, faces, skin, hair,eyes, selected.get()),
+    Button(ventana_gender, command=lambda: mostrarInfor(ventana_gender, cedula, skin, hair,eyes, selected.get()),
            text="Next",
            font="Arial, 11").pack(padx=10, pady=20)
 
@@ -347,11 +303,6 @@ def validarId(v,cedula):
             else:
                 MessageBox.showerror("Error", "THIS ID IS NOT REGISTERED")
 
-def mostrarRostro(faces):
-
-    rostros = Rostro()
-    return rostros.getRostroIndice(faces)
-
 def mostrarColor(skin):
 
     piel = Piel()
@@ -362,10 +313,10 @@ def mostrarCabello(hair):
     cabello = Cabello()
     return cabello.getTexturaIndice(hair)
 
-def mostrarOjos(eyes):
+def mostrarOjos(hairs):
 
-    ojos = Ojos()
-    return ojos.getOjosIndice(eyes)
+    colorHairs = ColorCabello()
+    return colorHairs.getColorCabelloIndice(hairs)
 
 def mostrarGenero(gender):
 
@@ -373,25 +324,24 @@ def mostrarGenero(gender):
     return genero.getgeneros(gender)
 
 
-def guardarDatos(cedula,gender,faces,skin,hair,eyes):
+def guardarDatos(cedula,gender,skin,hair,eyes):
     nombreTxt = str(cedula)+".txt"
     archivo = open(nombreTxt,"w")
     archivo.write("INFORMATION AVATAR" + '\n')
     archivo.write("Genero " + str(gender) + '\n')
-    archivo.write("Rostro "+ str(faces) + '\n')
     archivo.write("Color Piel " + str(skin) + '\n')
     archivo.write("Pelo "+ str(hair) + '\n')
-    archivo.write("Ojos "+ str(eyes) + '\n')
+    archivo.write("Color Pelo "+ str(eyes) + '\n')
     archivo.close()
 
-def mostrarInfor(ventana,cedula,faces,skin,hair,eyes,gender):
+def mostrarInfor(ventana,cedula,skin,hair,eyes,gender):
 
     ventana_gender = Toplevel()
     ventana_gender.config(bg = "pink")
     ventana_gender.title("Create Avatar")
     ventana_gender.geometry("600x650")
 
-    face = mostrarRostro(faces)
+
     skins = mostrarColor(skin)
     hairs = mostrarCabello(hair)
     eye = mostrarOjos(eyes)
@@ -406,9 +356,6 @@ def mostrarInfor(ventana,cedula,faces,skin,hair,eyes,gender):
     Label(ventana_gender, bg="pink", text="ITS GENDER", font='Arial, 13').pack(padx=10, pady=10)
     Label(ventana_gender, bg="pink", text=genders, font="Arial, 12").pack()
 
-    Label(ventana_gender, bg="pink", text="YOUR SHAPE OF FACE ", font='Arial, 13').pack(padx=10, pady=10)
-    Label(ventana_gender, bg="pink", text=face, font="Arial, 12").pack()
-
     Label(ventana_gender, bg="pink", text="YOUR SKIN COLOR", font='Arial, 13').pack(padx=10, pady=10)
     Label(ventana_gender, bg="pink", text=skins, font="Arial, 12").pack()
 
@@ -421,7 +368,7 @@ def mostrarInfor(ventana,cedula,faces,skin,hair,eyes,gender):
     Label(ventana_gender, bg="pink", text="YOUR EYE SHAPE", font='Arial, 13').pack(padx=10, pady=10)
     Label(ventana_gender, bg="pink", text=eye, font="Arial, 12").pack()
 
-    guardarDatos(cedula,gender,faces,skin,hair,eyes)
+    guardarDatos(cedula,gender,skin,hair,eyes)
 
     Button(ventana_gender, text="Exit",font="Arial,11", command=lambda: admin(ventana_gender,cedula) ).pack(padx=0, pady=20)
 
@@ -593,10 +540,10 @@ def Consulta_1(carpeta,indice,comp):
             archi.close()
     return resultados
 
-a= Consulta_1("C:/Users/hp/Documents/GitHub/proyecto-graphic-avatar-simulator/AVATARS",2,"Rostro 3\n")
+a= Consulta_1('C:/Users/ma210/OneDrive/Documents/GitHub/proyecto-graphic-avatar-simulator/AVATARS',2,"Rostro 3\n")
 cont=0
 for i in a:
-    archivo= open("C:/Users/hp/Documents/GitHub/proyecto-graphic-avatar-simulator/AVATARS/"+a[cont])
+    archivo= open('C:/Users/ma210/OneDrive/Documents/GitHub/proyecto-graphic-avatar-simulator/AVATARS/'+a[cont])
     lineas= archivo.readlines()
     cont+=1
     print(lineas)
@@ -613,10 +560,10 @@ def Consulta_2(carpeta,indice,comp):
             archi.close()
     return resultados
 
-a= Consulta_2("C:/Users/hp/Documents/GitHub/proyecto-graphic-avatar-simulator/AVATARS",2,"Rostro 4\n")
+a= Consulta_2('C:/Users/ma210O/neDrive/Documents/GitHub/proyecto-graphic-avatar-simulator/AVATARS',2,"Rostro 4")
 cont=0
 for i in a:
-    archivo= open("C:/Users/hp/Documents/GitHub/proyecto-graphic-avatar-simulator/AVATARS/"+a[cont])
+    archivo= open('C:/Users/ma210O/neDrive/Documents/GitHub/proyecto-graphic-avatar-simulator/AVATARS/'+a[cont])
     lineas= archivo.readlines()
     cont+=1
     print(lineas)
